6cedbc761bf18e18ce2e5dc735141a2b236ff44b
Krzysztof Grzempa <krzysztof.grzempa@allegrogroup.com>
2018-01-03 23:49:56 +0100

    WIP: intend correction

--- src/ConfigLoader.cpp
+++ src/ConfigLoader.cpp
@@ -4,82 +4,77 @@
 #include <boost/program_options.hpp>
 #include <string>
 #include <fstream>
+#include <iostream>
 
 ConfigLoader::ConfigLoader(int ac, char** av)
 {
+    const std::string AUTHOR="Krzysztof Grzempa";
+    const std::string LICENCE="GNU GPLv3";
+    const std::string APPNAME="webgossipme";
+    const int YEAR=2016;
+    const int SUCCESS = 0;
 
+    namespace po = boost::program_options;
+    po::options_description desc("Available options");
+    po::positional_options_description posOpt;
+    po::options_description confdesc("Config Options");
 
-  const std::string AUTHOR="Krzysztof Grzempa";
-  const std::string LICENCE="GNU GPLv3";
-  const std::string APPNAME="webgossipme";
-  const int YEAR=2016;
-  const int SUCCESS = 0;
-
-  namespace po = boost::program_options;
-  po::options_description desc("Available options");
-  po::positional_options_description posOpt;
-  po::options_description confdesc("Config Options");
-
-  try
-  {
-    std::ifstream Config_File(av[1]);
-    desc.add_options()
-    ("about", "Authors and licences")
-    ("help,h", "produce help message")
-    ("version,v", "print version informations")
-    ("configfile", po::value<std::string>(), "Configuration file");
-    posOpt.add("configfile", -1);
-    po::variables_map vm;
-    po::store(po::command_line_parser(ac, av).options(desc).positional(posOpt).run(), vm);
-
-    if (vm.count("help"))
+    try
     {
-      //std::cout << desc << "\n";
-      rad::OptionPrinter::printStandardAppDesc(APPNAME, std::cout,
-          desc,
-          &posOpt);
+        std::ifstream Config_File(av[1]);
+        desc.add_options()
+        ("about", "Authors and licences")
+        ("help,h", "produce help message")
+        ("version,v", "print version informations")
+        ("configfile", po::value<std::string>(), "Configuration file");
+        posOpt.add("configfile", -1);
+        po::variables_map vm;
+        po::store(po::command_line_parser(ac, av).options(desc).positional(posOpt).run(), vm);
+
+        if (vm.count("help"))
+        {
+            //std::cout << desc << "\n";
+            rad::OptionPrinter::printStandardAppDesc(APPNAME, std::cout,
+                    desc,
+                    &posOpt);
+
+        }
+
+        if (vm.count("version"))
+        {
+            using AutoVersion::MINOR;
+            using AutoVersion::MAJOR;
+            using AutoVersion::BUILD;
+            using AutoVersion::REVISION;
+            using AutoVersion::STATUS;
+            using AutoVersion::STATUS_SHORT;
+            std::cout <<  MAJOR << "." << MINOR << "." << BUILD << STATUS_SHORT << " rev: " << REVISION << std::endl;
+        }
+
+        if (vm.count("about"))
+        {
+            std::cout << "Author: " << AUTHOR << " " << YEAR << "\n"
+                      << "Under licence: " << LICENCE << "\n";
+
+        }
+
+        if(vm.count("configfile"))
+        {
+            std::cout << "Z pliku loga" << vm["configfile"].as<std::string>();
+            confdesc.add_options()
+            ("LogFile", po::value<std::string>())
+            ("IgnoreFile", po::value<std::string>());
+            po::store(po::parse_config_file(Config_File, confdesc), vm);
+
+            std::cout << vm["LogFile"].as<std::string>();
+            std::cout << vm["IgnoreFile"].as<std::string>();
+        }
+
+        po::notify(vm);
 
     }
-
-    if (vm.count("version"))
+    catch(po::error & ba)
     {
-      using AutoVersion::MINOR;
-      using AutoVersion::MAJOR;
-      using AutoVersion::BUILD;
-      using AutoVersion::REVISION;
-      using AutoVersion::STATUS;
-      using AutoVersion::STATUS_SHORT;
-      std::cout <<  MAJOR << "." << MINOR << "." << BUILD << STATUS_SHORT << " rev: " << REVISION << std::endl;
-
+        std::cout << "ERROR: " << ba.what();
     }
-
-    if (vm.count("about"))
-    {
-      std::cout << "Author: " << AUTHOR << " " << YEAR << "\n"
-                << "Under licence: " << LICENCE << "\n";
-
-    }
-
-    if(vm.count("configfile"))
-    {
-      std::cout << "Z pliku loga" << vm["configfile"].as<std::string>();
-      confdesc.add_options()
-      ("LogFile", po::value<std::string>())
-      ("IgnoreFile", po::value<std::string>());
-      po::store(po::parse_config_file(Config_File, confdesc), vm);
-
-      std::cout << vm["LogFile"].as<std::string>();
-      std::cout << vm["IgnoreFile"].as<std::string>();
-    }
-
-    po::notify(vm);
-
-  }
-  catch(po::error & ba)
-  {
-
-    std::cout << "ERROR: " << ba.what();
-
-  }
-
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include "../include/ConfigLoader.h"
-#include "../../curlpp/include/curlpp/cURLpp.hpp"
-#include "../../curlpp/include/curlpp/Easy.hpp"
-#include "../../curlpp/include/curlpp/Options.hpp"
+//#include "../../curlpp/include/curlpp/cURLpp.hpp"
+//#include "../../curlpp/include/curlpp/Easy.hpp"
+//#include "../../curlpp/include/curlpp/Options.hpp"
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
 #include "../../KGrawler/include/KGrawler.h"
 
 int main(int argc, char** argv)
 {
-  //std::vector<char *> test_seed {"http://127.0.0.1", "http://127.0.0.1/next.html"};
-  std::vector<char *> test_seed {"https://www.onet.pl"};
-  ConfigLoader(argc, argv);
-  KGrawler Crawl(test_seed);
-  Crawl.start();
-  Crawl.stop();
+    //std::vector<char *> test_seed {"http://127.0.0.1", "http://127.0.0.1/next.html"};
+    std::vector<char *> test_seed {"https://www.tvn24.pl","https://www.onet.pl", "https://wp.pl"};
+    ConfigLoader(argc, argv);
+    KGrawler Crawl(test_seed);
+    Crawl.start();
+    Crawl.stop();
 }
